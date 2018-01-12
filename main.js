7cd1e555c36284c80bbd13344d686583d4cdaa4e
Stephen Woosley <stephenrwoosley@gmail.com>
2018-01-12 11:44:57 -0500

    api call working

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const key = keyHolder.NYT_KEY;
 let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 let queryString = '';
-let startYear = '';
-let endYear = '';
+let startYear = 0;
+let endYear = 0;
 
 const clearForm = () => {
   $("#search-term").val('');
@@ -11,32 +11,52 @@ const clearForm = () => {
   $("#end-year").val('');
 }
 
+const formatYear = (year) => {
+  year === 2018
+    ? year = moment().format('YYYYMMDD')
+    : year = `${year}0101`
+  return year;
+}
+
 
 $("#search-btn").on("click", (e) => {
   e.preventDefault();
-  console.log("clicked search button!");
-  // $(".article-list").html("<h1>jQuery Working!</h1");
+
+  // console.log(`Start year field is ${$("#start-year").val()}; end year field is ${$("#end-year").val()}`)
+  // console.log(`Start year is ${startYear}; end year is ${endYear}`);
 
   queryString = $("#search-term").val();
-  startYear = $("#start-year").val();
-  endYear = $("#end-year").val();
+
+  // if start-year field is empty
+  !$("#start-year").val()
+    // yes? set startYear to 1900
+    ? startYear = formatYear(1900)
+    // no? set startYear to a formatted version of the value of start-year
+    : startYear = formatYear($("#start-year").val())
+    
+  // if end-year field is empty
+  !$("#end-year").val()
+    // yes? set endYear to a formatted version of the current year
+    ? endYear = formatYear(moment().format('YYYY'))
+    // no? set endYear to a formatted version of the value of end-year
+    : endYear = formatYear($("#end-year").val()) 
+
+  console.log(`Start year is ${startYear}; end year is ${endYear}`);
 
   url += '?' + $.param({
+    'api-key': key,
     'q': queryString,
     'begin_date': startYear,
     'end_date': endYear
   });
 
   console.log(url);
-  clearForm();
 
-  // $.ajax({
-  //   url: url,
-  //   method: 'GET',
-  // }).done(function(result) {
-  //   console.log(result);
-  // }).fail(function(err) {
-  //   throw err;
-  // });
+  $.ajax({
+    method: 'GET',
+    url: url
+  }).done((result) => {
+    console.log(result.response.docs[0].headline.print_headline);
+  })
 });
 
